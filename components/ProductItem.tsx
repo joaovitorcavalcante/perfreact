ab70e4d452c350728cc3b7d4192e782a20f13010
João Vitor <contato.cavalcantebarbosa@gmail.com>
2022-04-21 10:07:53 -0300

    updates

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,23 +1,53 @@
-import { memo } from "react";
+import dynamic from "next/dynamic";
+import { memo, useState } from "react";
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  () => {
+    return import("./AddProductToWishlist").then(
+      (mod) => mod.AddProductToWishlist
+    );
+  },
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
 
 type ProductItemProps = {
   product: {
     id: number;
     price: number;
     title: string;
+    priceFormatted: string;
   };
   onAddToWishlist: (id: number) => void;
 };
 
-export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+export function ProductItemComponent({
+  product,
+  onAddToWishlist,
+}: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWithlist] = useState(false);
+
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
-      <button onClick={() => onAddToWishlist(product.id)}></button>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWithlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWithlist(false)}
+        />
+      )}
     </div>
   );
 }
 
-export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps.product, nextProps.product);
-});
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,17 @@ import type { NextPage } from "next";
 import { FormEvent, useCallback, useState } from "react";
 import { SearchResults } from "../components/SearchResults";
 
+type ResultsData = {
+  totalPrice: number;
+  data: any[];
+};
+
 const Home: NextPage = () => {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<ResultsData>({
+    totalPrice: 0,
+    data: [],
+  });
 
   const addToWishlist = useCallback(async (id: number) => {
     console.log(id);
@@ -20,7 +28,21 @@ const Home: NextPage = () => {
     const response = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await response.json();
 
-    setResults(data);
+    const formatter = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    const products = data.map((product) => ({
+      ...product,
+      priceFormatted: formatter.format(product.price),
+    }));
+
+    const totalPrice = data.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+
+    setResults({ totalPrice, data: products });
   }
 
   return (
@@ -35,10 +57,11 @@ const Home: NextPage = () => {
         />
       </form>
 
-      <SearchResults 
-        onAddToWishlist={addToWishlist} 
-        results={results}
-       />
+      <SearchResults
+        onAddToWishlist={addToWishlist}
+        results={results.data}
+        totalPrice={results.totalPrice}
+      />
     </div>
   );
 };
